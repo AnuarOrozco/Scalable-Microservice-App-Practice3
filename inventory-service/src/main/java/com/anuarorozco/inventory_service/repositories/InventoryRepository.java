76b61286902b7f552c966574d51edb48c6af4fb5
Anuar Orozco <anuarorozzco@gmail.com>
2024-12-03 10:56:23 -0600

    inventory-service: Created controller, repo and service, copied and pasted dtos from Order microservice

--- inventory-service/src/main/java/com/anuarorozco/inventory_service/repositories/InventoryRepository.java
+++ inventory-service/src/main/java/com/anuarorozco/inventory_service/repositories/InventoryRepository.java
@@ -4,6 +4,12 @@ import com.anuarorozco.inventory_service.model.entities.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySku(String sku);
+
+    List<Inventory> findBySkuIn(List<String> skus);
 }
